daf1ff26b9d79f3d0001724de213a1dc1fc3c2cd
khrsany <alabass.alkhrsany.02@gmail.com>
2025-03-07 14:44:12 +0200

    Refactored resource distribution: moved max_values to Helpers.py

--- src/Helpers.py
+++ src/Helpers.py
@@ -6,9 +6,25 @@ def get_random_airport(airports):
     return random_airport
 
 
+
+# resurssit
 inventory_items = [
     'fuel',
-    'health',
+    'medical',
     'food',
-    'weapon'
+    'weapon',
+    'water',
+    'tools',
+    'clothing'
 ]
+
+# Maksimiarvot
+max_values = {
+    'weapon': 5,
+    'medical': 50,
+    'food': 50,
+    'fuel': 50,
+    'water': 50,
+    'tools': 3,
+    'clothing': 10
+}
