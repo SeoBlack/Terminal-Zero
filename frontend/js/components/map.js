912ad84a9cb4bb687230ce0f6a158ba92c2f0cd3
SeoBlack <sooreennadum2001@gmail.com>
2025-05-01 11:39:13 +0300

    create initial game screen

---
+++ frontend/js/components/map.js
@@ -0,0 +1,4 @@
+var map = L.map('map-view').setView([62.505, 23], 13);
+
+
+ L.tileLayer('https://tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=Q7LqsU4uCRpBRQmdA0wCfMBqoKmlramXUXl59KMEPYUzw4pdB7m4QLUATbSQwO92', {}).addTo(map);
