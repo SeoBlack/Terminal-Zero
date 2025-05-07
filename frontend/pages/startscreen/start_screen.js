7a743bb4eb086e3a4b257fd4425e28a30332bc47
SeoBlack <sooreennadum2001@gmail.com>
2025-05-07 22:41:30 +0300

    fix delete button icon

--- frontend/pages/startscreen/start_screen.js
+++ frontend/pages/startscreen/start_screen.js
@@ -5,6 +5,7 @@ import {getAllGames, setCurrentUser} from "../../js/components/localstorage.js";
 import {playSoundEffect, soundEffects} from "../../js/components/sound_effects.js";
 import {hideLoadingDialog, showLoadingDialog} from "../../js/components/loading.js";
 import {showConfirmationDialog} from "../../js/components/confirmation_dialog.js";
+import {Icons} from "../../js/components/icons.js";
 
 function startScreen(){
     // --------------VARIABLES----------------
@@ -18,15 +19,20 @@ function startScreen(){
     previousPlayersList.innerHTML = '<p id="info-text">or continue where you left</p>';
     previousPlayers.forEach(player => {
         const playerDiv = document.createElement('div');
-        playerDiv.className = 'flex row username-container';
+        playerDiv.className = 'flex row';
+        playerDiv.style.alignItems = "center";
         playerDiv.innerHTML = `
-            <span class="delete-button" data-username="${player}" style="cursor: pointer;">🗑️</span>
+            <span class="delete-button trash" data-username="${player}" style="cursor: pointer;">${Icons.trash}</span>
+            <div class="username-container flex row">
+            
             <p class="username-text">${player}</p>
             <svg class="arrow w-6 h-6 text-gray-800 dark:text-white" xmlns="http://www.w3.org/2000/svg"
                  width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                       d="M19 12H5m14 0-4 4m4-4-4-4"/>
             </svg>
+            </div>
+
         `;
 
         // Pelin aloitus kun nimeä klikataan
